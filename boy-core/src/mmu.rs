06a8eb43e99f723636e5d356f6e41d1c8f2d1659
Tobias Bordenca <tobiasuzt@gmail.com>
2025-12-22 15:03:36 +0100

    Add write operation to MMU

--- boy-core/src/mmu.rs
+++ boy-core/src/mmu.rs
@@ -36,7 +36,7 @@ impl MMU {
             0xFF00..=0xFF7F => self.io[(addr - 0xFF00) as usize],
             0xFF80..=0xFFFE => self.hram[(addr - 0xFF80) as usize],
             0xFFFF => self.ie,
-            _ => 0, // Unmapped memory returns 0
+            _ => 0xFF, // Unmapped memory returns 0xFF
         }
     }
 
@@ -44,5 +44,27 @@ impl MMU {
         let lo = self.rb(addr) as u16;
         let hi = self.rb(addr.wrapping_add(1)) as u16;
         (hi << 8) | lo
+    }
+
+    #[inline]
+    pub fn wb(&mut self, addr: u16, value: u8) {
+        match addr {
+            // 0x0000..=0x7FFF => (),
+            0x8000..=0x9FFF => self.vram[(addr - 0x8000) as usize] = value,
+            0xA000..=0xBFFF => self.eram[(addr - 0xA000) as usize] = value,
+            0xC000..=0xDFFF => self.wram[(addr - 0xC000) as usize] = value,
+            0xE000..=0xFDFF => self.wram[(addr - 0xE000) as usize] = value,
+            0xFE00..=0xFE9F => self.oam[(addr - 0xFE00) as usize] = value,
+            // 0xFEA0..=0xFEFF => (),
+            0xFF00..=0xFF7F => self.io[(addr - 0xFF00) as usize] = value,
+            0xFF80..=0xFFFE => self.hram[(addr - 0xFF80) as usize] = value,
+            0xFFFF => self.ie = value,
+            _ => (), // Unwritable memory
+        }
+    }
+
+    pub fn ww(&mut self, addr: u16, value: u16) {
+        self.wb(addr, (value & 0x00FF) as u8);
+        self.wb(addr.wrapping_add(1), (value >> 8) as u8);
     }
 }
